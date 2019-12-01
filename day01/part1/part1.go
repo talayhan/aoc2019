8cb14307cdf48c786cde7d08477431bb3340349d
talayhan <samet.talayhan@gmail.com>
2019-12-01 17:54:29 +0300

    Add day01

---
+++ day01/part1/part1.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var num int
+	var sum int
+
+	for {
+
+		_, err := fmt.Fscanf(file, "%d\n", &num) // give a patter to scan
+
+		if err != nil {
+
+			if err == io.EOF {
+				break // stop reading the file
+			}
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		sum += (num / 3) - 2
+	}
+
+	// print out the sum
+	fmt.Println(sum)
+}
